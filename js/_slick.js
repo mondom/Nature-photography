4a30c55f7d076cd1495582e86758a85cd45500bb
Monika <codingbymon@gmail.com>
2023-05-22 14:19:39 +0200

    finish my first simple page

--- js/_slick.js
+++ js/_slick.js
@@ -1,7 +1,46 @@
-// $('.carousel').slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     fade: true,
-//     cssEase: 'linear'
-//   });
+$('.gallery-box').slick({
+	centerMode: true,
+	centerPadding: '60px',
+	slidesToShow: 2,
+	autoplay: true,
+	autoplaySpeed: 3000,
+	arrows: false,
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 2,
+			},
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 2,
+			},
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 1,
+			},
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 1,
+			},
+		},
+	],
+})
--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ const burgerBtn = document.querySelector('.header__burger-icon')
 const closeNavBtn = document.querySelector('.nav-mobile__close-icon')
 const mobileNav = document.querySelector('.nav-mobile')
 const allNavItems = document.querySelectorAll('.nav__link')
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
     mobileNav.classList.toggle('nav-mobile--active')
+
+	document.body.classList.toggle('sticky-body')
 
     allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
@@ -16,3 +19,9 @@ const handleNav = () => {
 
 burgerBtn.addEventListener('click', handleNav)
 closeNavBtn.addEventListener('click', handleNav)
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+handleCurrentYear()
