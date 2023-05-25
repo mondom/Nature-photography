b7ec080afd1cc199fc9d826c98d276a64f6da1b0
Monika <codingbymon@gmail.com>
2023-05-25 15:38:30 +0200

    new changes

--- js/slick.js
+++ js/slick.js
@@ -1,46 +1,34 @@
 $('.gallery-box').slick({
-	centerMode: true,
-	centerPadding: '60px',
-	slidesToShow: 2,
-	autoplay: true,
-	autoplaySpeed: 3000,
+	dots: false,
 	arrows: false,
+	infinite: true,
+	autoplay: true,
+	speed: 400,
+	slidesToShow: 3,
+	slidesToScroll: 1,
 	responsive: [
-		{
-			breakpoint: 992,
-			settings: {
-				arrows: false,
-				centerMode: true,
-				centerPadding: '40px',
-				slidesToShow: 2,
-			},
-		},
-		{
-			breakpoint: 768,
-			settings: {
-				arrows: false,
-				centerMode: true,
-				centerPadding: '40px',
-				slidesToShow: 2,
-			},
-		},
-		{
-			breakpoint: 768,
-			settings: {
-				arrows: false,
-				centerMode: true,
-				centerPadding: '40px',
-				slidesToShow: 1,
-			},
-		},
-		{
-			breakpoint: 480,
-			settings: {
-				arrows: false,
-				centerMode: true,
-				centerPadding: '40px',
-				slidesToShow: 1,
-			},
-		},
-	],
-})
+	  {
+		breakpoint: 1024,
+		settings: {
+		  slidesToShow: 2,
+		  slidesToScroll: 1,
+		  infinite: true,
+		  dots: false
+		}
+	  },
+	  {
+		breakpoint: 600,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1
+		}
+	  },
+	  {
+		breakpoint: 480,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1
+		}
+	  }
+	]
+  });
